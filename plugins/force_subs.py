4a9c6c6aa7975de98191698840bf02bf466ba338
Techify Bots <TechifyBots@gmail.com>
2025-06-19 17:59:19 +0530

    Update force_subs.py

--- plugins/force_subs.py
+++ plugins/force_subs.py
@@ -1,43 +1,32 @@
-from pyrogram import Client, filters, enums 
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.errors import UserNotParticipant
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import Config
-from helper.database import jishubotz
+from pyrogram import Client
+from pyrogram.types import Message
+from typing import List
+from pyrogram.errors import UserNotParticipant
 
-
-
-async def not_subscribed(_, client, message):
-    await jishubotz.add_user(client, message)
-    if not Config.FORCE_SUB:
+async def get_fsub(bot: Client, message: Message) -> bool:
+    tb = await bot.get_me()
+    user_id = message.from_user.id
+    not_joined_channels = []
+    for channel_id in Config.AUTH_CHANNELS:
+        try:
+            await bot.get_chat_member(channel_id, user_id)
+        except UserNotParticipant:
+            chat = await bot.get_chat(channel_id)
+            invite_link = chat.invite_link or await bot.export_chat_invite_link(channel_id)
+            not_joined_channels.append((chat.title, invite_link))
+    if not_joined_channels:
+        join_buttons = []
+        for i in range(0, len(not_joined_channels), 2):
+            row = []
+            for j in range(2):
+                if i + j < len(not_joined_channels):
+                    title, link = not_joined_channels[i + j]
+                    button_text = f"{i + j + 1}. {title}"
+                    row.append(InlineKeyboardButton(button_text, url=link))
+            join_buttons.append(row)
+        join_buttons.append([InlineKeyboardButton("🔄 Try Again", url=f"https://telegram.me/{tb.username}?start=start")])
+        await message.reply(f"**🎭 {message.from_user.mention}, As I see, you haven’t joined my channel yet.\nPlease join by clicking the button below.**", reply_markup=InlineKeyboardMarkup(join_buttons))
         return False
-    try:             
-        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id) 
-        if user.status == enums.ChatMemberStatus.BANNED:
-            return True 
-        else:
-            return False                
-    except UserNotParticipant:
-        pass
     return True
-
-
-@Client.on_message(filters.private & filters.create(not_subscribed))
-async def forces_sub(client, message):
-    buttons = [[InlineKeyboardButton(text="🔺 Update Channel 🔺", url=f"https://telegram.me/{Config.FORCE_SUB}") ]]
-    text = f"""<b>Hello {message.from_user.mention} \n\nYou Need To Join In My Channel To Use Me\n\nKindly Please Join Channel</b>"""
-    try:
-        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)    
-        if user.status == enums.ChatMemberStatus.BANNED:                                   
-            return await client.send_message(message.from_user.id, text="Sorry You Are Banned To Use Me")  
-    except UserNotParticipant:                       
-        return await message.reply_text(text=text,quote=True, reply_markup=InlineKeyboardMarkup(buttons))
-    return await message.reply_text(text=text,quote=True, reply_markup=InlineKeyboardMarkup(buttons))
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @JishuBotz
-# Developer @JishuDeveloper
